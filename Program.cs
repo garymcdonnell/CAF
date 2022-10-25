c67322fb05fc7b58dfde519afc7db64e011f880c
Gary <Gary@W10DEV03>
2022-10-25 12:37:33 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.IO;
+
+namespace CAF
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            //This program creates a folder using the current date as the name (format "YYYY-MM-DD")
+
+            DateTime CurrentDate = System.DateTime.Today.Date;
+            DateTime DateToUse = CurrentDate;
+
+            Int32 Year = DateToUse.Year;
+            Int32 Month = DateToUse.Month;
+            Int32 Day = DateToUse.Day;
+            string FolderName = string.Format("{0:0000}", Year) + "-" + string.Format("{0:00}", Month) + "-" + string.Format("{0:00}", Day);
+
+            string FolderToCreate = "";
+
+            if (args.Length == 0)
+            {
+                Directory.CreateDirectory(FolderName);
+            }
+            else
+            {
+                if ((args[0] == "/?") | (args[0] == "?"))
+                {
+                    Console.WriteLine("Create folder using current date (\"YYYY-MM-DD\") as name. Defaults to current folder or specify path on command line.");
+                }
+                else
+                {
+                    FolderToCreate = string.Concat(args[0], @"\", FolderName);
+                    Directory.CreateDirectory(FolderToCreate);
+                }
+            }
+        }
+    }
+}
